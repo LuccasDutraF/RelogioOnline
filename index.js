3948d665a136572dd21b10bc2784a00bade1d008
Luccas Dutra <luccas.ferreyro@gmail.com>
2023-07-20 16:28:07 -0300

    .

---
+++ index.js
@@ -0,0 +1,55 @@
+function carregar(){
+    var msg = window.document.getElementById('mensagem')
+    var img = window.document.getElementById('imagem')
+    var msg2 = window.document.getElementById('mensagem2')
+    var espaço = window.document.getElementById('espaço')
+    var titulo = window.document.getElementById('titulo')
+    var rodape = window.document.getElementById('rodape')
+    var data = new Date()
+    var hora = data.getHours()
+    var minute = new Date()
+    var minuto = minute.getMinutes()
+    msg.innerHTML =`Agora são ${hora} horas e ${minuto} minuto`
+    
+    if (hora >= 0 && hora <5){
+        img.src= '/Circulo-horario/fotomadruga.png'
+        msg2.innerHTML = "Boa Madrugada para você"
+        document.body.style.backgroundImage = "url('/Fundo-horario/madrugada.jpg')"
+        msg.style.color = ("white")
+        msg2.style.color = ("white")
+        titulo.style.color = ("white")
+        rodape.style.color = ("white")
+        espaço.style.backgroundColor = ("#6c5ae750")
+    }
+    else if (hora >=6 && hora <=12){
+        img.src = '/Circulo-horario/fotomanha.png'
+        msg2.innerHTML = "Bom Dia para você"
+        document.body.style.backgroundImage = "url('/Fundo-horario/fundo.jpg')"
+        espaço.style.backgroundColor = ("#00b0c150")
+    } else if 
+        (hora >12 && hora <=18){
+        img.src = '/Circulo-horario/fototarde.png'
+        msg2.innerHTML = "Boa Tarde para você"
+        document.body.style.backgroundImage = "url('/Fundo-horario/tarde.jpg')"
+        espaço.style.backgroundColor = ("#971b0750")
+    } else{
+        img.src = '/Circulo-horario/fotonoite.png'
+        msg2.innerHTML = "Boa Noite para Você"
+        document.body.style.backgroundImage = "url('/Fundo-horario/noite.jpg')"
+        espaço.style.backgroundColor = ("#2f02b250")
+        msg.style.color = ("white")
+        msg2.style.color = ("white")
+        titulo.style.color = ("white")
+        rodape.style.color = ("white")
+        }
+
+        if(minuto >1){
+        msg.innerHTML = `Agora são ${hora} horas e ${minuto} minutos`
+        }
+
+        if(minuto ==0){
+        msg.innerHTML = `Agora são ${hora} horas em ponto`
+        }
+
+}
+ 
