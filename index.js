f1be554bf4a00358b60e30426233f5f1621f67fb
Luccas Dutra <luccas.ferreyro@gmail.com>
2023-07-21 13:24:05 -0300

    atualização de escritura

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ function carregar(){
     var msg = window.document.getElementById('mensagem')
     var img = window.document.getElementById('imagem')
     var msg2 = window.document.getElementById('mensagem2')
-    var espaço = window.document.getElementById('espaço')
+    var espaço = window.document.getElementById('espaçocontainer')
     var titulo = window.document.getElementById('titulo')
     var rodape = window.document.getElementById('rodape')
     var data = new Date()
     var hora = data.getHours()
     var minute = new Date()
     var minuto = minute.getMinutes()
-    msg.innerHTML =`Agora são ${hora} horas e ${minuto} minuto`
+    msg.innerHTML =`Agora são ${hora} horas e ${minuto} minutos`
     
     if (hora >= 0 && hora <5){
         img.src= 'fotomadruga.png'
@@ -19,32 +19,32 @@ function carregar(){
         msg2.style.color = ("white")
         titulo.style.color = ("white")
         rodape.style.color = ("white")
-        espaço.style.backgroundColor = ("#6c5ae750")
+        espaçocontainer.style.backgroundColor = ("#6c5ae750")
     }
     else if (hora >=6 && hora <=12){
         img.src = 'fotomanha.png'
         msg2.innerHTML = "Bom Dia para você"
         document.body.style.backgroundImage = "url('fundo.jpg')"
-        espaço.style.backgroundColor = ("#00b0c150")
+        espaçocontainer.style.backgroundColor = ("#00b0c150")
     } else if 
         (hora >12 && hora <=18){
         img.src = 'fototarde.png'
         msg2.innerHTML = "Boa Tarde para você"
         document.body.style.backgroundImage = "url('tarde.jpg')"
-        espaço.style.backgroundColor = ("#971b0750")
+        espaçocontainer.style.backgroundColor = ("#971b0750")
     } else{
         img.src = 'fotonoite.png'
         msg2.innerHTML = "Boa Noite para Você"
         document.body.style.backgroundImage = "url('noite.jpg')"
-        espaço.style.backgroundColor = ("#2f02b250")
+        espaçocontainer.style.backgroundColor = ("#2f02b250")
         msg.style.color = ("white")
         msg2.style.color = ("white")
         titulo.style.color = ("white")
         rodape.style.color = ("white")
         }
 
-        if(minuto >1){
-        msg.innerHTML = `Agora são ${hora} horas e ${minuto} minutos`
+        if(minuto ==1){
+        msg.innerHTML = `Agora são ${hora} horas e ${minuto} minuto`
         }
 
         if(minuto ==0){
